e3238b3845e2543eaca306682fbd3bfa80e6c402
Arthur <arthursouzafut22@gmail.com>
2024-10-12 19:17:53 -0300

    check item quantity

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const listaItems = document.querySelector(".container-compras");
 let input = document.querySelector(".input");
 const form = document.querySelector(".form");
 const check = document.querySelector(".check");
+const invalid = document.querySelector(".invalid");
 const imgsIcon = ["imgs/delete.svg", "imgs/edit.svg"];
 
 // Adicionar item a lista....
 const newArray = [];
 
-function onSubitItem(event) {
+function addNewItem(event) {
   event.preventDefault();
   if (input.value.trim() === "") return alert("Campo vazio!");
 
@@ -20,21 +21,20 @@ function onSubitItem(event) {
   }
 
   input.value = "";
+  checkContainer();
 }
 
-form.addEventListener("submit", onSubitItem);
-
+form.addEventListener("submit", addNewItem);
 
 // Validar se tem item na lista...
-function checkContainer() {
-  if (newArray.length === 0) {
-    check.innerHTML = "Nenhum item na lista.";
-  } else if (newArray.length > 0) {
-    check.innerHTML = "";
-  }
-}
-checkContainer();
-
+// function checkContainer() {
+//   if (newArray.length === 0) {
+//     check.innerHTML = "Nenhum item na lista.";
+//   } else if (newArray.length > 0) {
+//     check.innerHTML = "";
+//   }
+// }
+// checkContainer();
 
 // New element....
 function newItem() {
@@ -50,7 +50,7 @@ function cardItem({ id, nome, data }) {
   return `<div class="card-item" id="${id}">
             <div class="wrapper-1">
               <div class="details">
-                <input type="checkbox" name="checkbox" id="checkbox" />
+                <input type="checkbox" name="checkbox" id="checkbox" onclick="checkBoxItem(event)"/>
                 <p>${nome}</p>
               </div>
               <div class="wrapper-btns">
@@ -65,9 +65,20 @@ function cardItem({ id, nome, data }) {
           </div>`;
 }
 
+// checkar input box...
+function checkBoxItem(event) {
+  const check = event.target.checked;
+
+  return check
+    ? alert("Deseja marca esse item?")
+    : !check
+    ? alert("Deseja desmarca esse item?")
+    : "";
+}
+
 //Remove Item....
 function removeItem(id) {
-  const index = newArray.findIndex((item) => item.id === id);
+  const index = newArray.findIndex((item) => item?.id === id);
   if (index !== -1) {
     newArray.splice(index, 1);
 
@@ -76,21 +87,23 @@ function removeItem(id) {
       listaItems.innerHTML += cardItem(item);
     }
   }
+
+  checkContainer();
 }
 
 //Editar Item...
 function editItem(nome) {
   const name = prompt("Digite o novo nome do item:");
-  const nameItem = newArray.findIndex((item) => item.nome === nome);
+  const nameItem = newArray.findIndex((item) => item?.nome === nome);
 
-  const itemEdit = {
+  const newEdit = {
     id: nameItem?.id,
-    nome: String(nameItem.nome).replace(nameItem.nome, name),
+    nome: String(nameItem?.nome).replace(nameItem?.nome, name),
     data: dayOfTheWeek() + hoursDays(),
   };
 
   if (nameItem !== -1) {
-    newArray.splice(nameItem, 1, itemEdit);
+    newArray.splice(nameItem, 1, newEdit);
     listaItems.innerHTML = "";
 
     for (const item of newArray) {
@@ -139,3 +152,11 @@ function dayOfTheWeek() {
   return dia;
 }
 
+// Verificar se tem item no array...
+function checkContainer() {
+  return newArray.length > 0
+    ? (check.style.display = "none")
+    : newArray.length === 0
+    ? (check.style.display = "block")
+    : null;
+}
